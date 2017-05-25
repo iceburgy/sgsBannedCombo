defdb28cc832ed70d4e5c376c7e338d54d6469e4
Ye Wang <ywang1@SEA.CORP.EXPECN.com>
2017-05-25 13:07:03 -0700

    more features

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # sgsBannedCombo
 三国杀禁将表
+
+Feature implemented:
+- Mobile friendliness
+- Element centered
+- Increase hint size
--- js/the-basics.js
+++ js/the-basics.js
@@ -1,64 +1,76 @@
-$(document).ready(function(){
-var substringMatcher = function(strs) {
-  return function findMatches(q, cb) {
-    var matches, substringRegex;
+$(document).ready(function () {
+  var goodtogo="无禁将组合，请放心使用！";
+  var substringMatcher = function (strs) {
+    return function findMatches(q, cb) {
+      var matches, substringRegex;
 
-    // an array that will be populated with substring matches
-    matches = [];
+      // an array that will be populated with substring matches
+      matches = [];
 
-    // regex used to determine if a string contains the substring `q`
-    substrRegex = new RegExp(q, 'i');
+      // regex used to determine if a string contains the substring `q`
+      substrRegex = new RegExp(q, 'i');
 
-    // iterate through the pool of strings and for any string that
-    // contains the substring `q`, add it to the `matches` array
-    $.each(strs, function(i, str) {
-      if (substrRegex.test(str)) {
-        matches.push(str);
-      }
-    });
+      // iterate through the pool of strings and for any string that
+      // contains the substring `q`, add it to the `matches` array
+      $.each(strs, function (i, str) {
+        if (substrRegex.test(str)) {
+          matches.push(str);
+        }
+      });
 
-    cb(matches);
+      cb(matches);
+    };
   };
-};
 
-$('.typeahead').typeahead({
-  hint: false,
-  highlight: true,
-  minLength: 1
-},
-{
-  name: 'bannedKeys',
-  source: substringMatcher(bannedKeys)
-});
+  $('.typeahead').typeahead({
+        hint: false,
+        highlight: true,
+        minLength: 1
+      },
+      {
+        name: 'bannedKeys',
+        source: substringMatcher(bannedKeys),
+        limit: 20
+      }
+  );
 
-function selectHandler(obj, datum, name) {
-	var key=datum;	
-	var banned="";
-	if($.inArray( key, bannedKeys )>=0){
-		if(dict[key]){
-			banned=dict[key];	
-		}else{
-			banned="无禁将组合";
-		}		
-	}
-  $( "#outputname" ).text(banned);
-}
-$('.typeahead').bind('typeahead:selected', selectHandler);
+  function selectHandler(obj, datum, name) {
+    var key = datum;
+    var banned = "";
+    if ($.inArray(key, bannedKeys) >= 0) {
+      if (dict[key]) {
+        banned = dict[key];
+      } else {
+        banned = goodtogo;
+      }
+    }
+    $("#outputwrapper").text(banned);
+    $("#btnClear").focus();
+  }
+  $('.typeahead').bind('typeahead:selected', selectHandler);
 
-function arrowHandler(number) {
-	var key=$(".typeahead").val();	
-	var banned="";
-	if($.inArray( key, bannedKeys )>=0){
-		if(dict[key]){
-			banned=dict[key];	
-		}else{
-			banned="无禁将组合";
-		}
-		
-	}
-  $( "#outputname" ).text(banned);
-}
-$(".typeahead").on('change keyup paste mouseup',arrowHandler);
+  function arrowHandler(number) {
+    var key = $(".typeahead").val();
+    var banned = "";
+    if ($.inArray(key, bannedKeys) >= 0) {
+      if (dict[key]) {
+        banned = dict[key];
+      } else {
+        banned = goodtogo;
+      }
+
+    }
+    $("#outputwrapper").text(banned);
+  }
+  $(".typeahead").on('change keyup paste mouseup', arrowHandler);
+
+  $("#btnClear").click(function() {
+    $(".typeahead").val("");
+    $("#outputwrapper").text("");
+    $(".typeahead").focus();
+  });
+
+  $(".typeahead").focus();
 
 });
 
