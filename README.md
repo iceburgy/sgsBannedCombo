1335b527c3dc2415d32914b167011b352fff31c4
Ye Wang <ywang1@expedia.com>
2017-05-25 18:09:22 -0700

    more features

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # sgsBannedCombo
 三国杀禁将表
 
-Feature implemented:
+# Feature implemented:
 - Mobile friendliness
 - Element centered
 - Increase hint size
+- Support search by initials (first letter only in multi-char last name)
+- Importing wujiang list from google sheets (shared for co-edit, must follow the same format)
+    - Wujiang base pool:
+        https://docs.google.com/spreadsheets/d/1_Wel_JJ-zD-mL8AKWb_P3dhJiQeERwdgwtTMVXMv2mI/edit?usp=sharing
+    - Wujiang banning map:
+        https://docs.google.com/spreadsheets/d/1oQ6G8YwcG1d53P8MBtsAmN2PI-MtzvVZYvcb8HBRHWs/edit?usp=sharing
--- js/the-basics.js
+++ js/the-basics.js
@@ -1,5 +1,54 @@
 $(document).ready(function () {
   var goodtogo="无禁将组合，请放心使用！";
+  var bannedKeys = [];
+  var bannedMap = new Map();
+
+  readSgsWujiangSet();
+  readSgsWujiangMap();
+  function populateBannedSet(bs) {
+    bannedKeys=bs;
+  }
+  function readSgsWujiangSet()
+  {
+    $.ajax("https://spreadsheets.google.com/spreadsheet/pub?key=1_Wel_JJ-zD-mL8AKWb_P3dhJiQeERwdgwtTMVXMv2mI&output=csv").done(function(result){
+      var bs=result.split("\n");
+      for (var i = 0; i < bs.length; i++) {
+        bs[i]=bs[i].replace(/\r?\n|\r/g, " ").trim();
+      }
+
+      populateBannedSet(bs);
+      $('.typeahead').typeahead({
+            hint: false,
+            highlight: true,
+            minLength: 1
+          },
+          {
+            name: 'bannedKeys',
+            source: substringMatcher(bs),
+            limit: 20
+          }
+      );
+    });
+  }
+
+  function populateBannedMap(bm) {
+    bannedMap=bm;
+  }
+
+  function readSgsWujiangMap()
+  {
+    $.ajax("https://spreadsheets.google.com/spreadsheet/pub?key=1oQ6G8YwcG1d53P8MBtsAmN2PI-MtzvVZYvcb8HBRHWs&output=csv").done(function(result){
+      var bm=new Map();
+      var rows = result.split("\n");
+      for (var i = 0; i < rows.length; i++) {
+        var cells = rows[i].split(",");
+        bm.set(cells[0].replace(/\r?\n|\r/g, " ").trim(), cells[1].replace(/\r?\n|\r/g, " ").trim());
+
+      }
+      populateBannedMap(bm);
+    });
+  }
+
   function isUpperCase(aCharacter)
   {
     return (aCharacter >= 'A') && (aCharacter <= 'Z');
@@ -38,24 +87,12 @@ $(document).ready(function () {
     };
   };
 
-  $('.typeahead').typeahead({
-        hint: false,
-        highlight: true,
-        minLength: 1
-      },
-      {
-        name: 'bannedKeys',
-        source: substringMatcher(bannedKeys),
-        limit: 20
-      }
-  );
-
   function selectHandler(obj, datum, name) {
-    var key = datum;
+    var key = datum.replace(/\r?\n|\r/g, " ").trim();
     var banned = "";
     if ($.inArray(key, bannedKeys) >= 0) {
-      if (dict[key]) {
-        banned = dict[key];
+      if (bannedMap.get(key)) {
+        banned = bannedMap.get(key);
       } else {
         banned = goodtogo;
       }
@@ -65,12 +102,12 @@ $(document).ready(function () {
   }
   $('.typeahead').bind('typeahead:selected', selectHandler);
 
-  function arrowHandler(number) {
+  function arrowHandler() {
     var key = $(".typeahead").val();
     var banned = "";
     if ($.inArray(key, bannedKeys) >= 0) {
-      if (dict[key]) {
-        banned = dict[key];
+      if (bannedMap.get(key)) {
+        banned = bannedMap.get(key);
       } else {
         banned = goodtogo;
       }
