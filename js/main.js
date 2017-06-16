8eff8c36b693203b4eb7a67c4f321e379ca82ad5
ice burg <iceburg2005@hotmail.com>
2017-06-15 20:29:51 -0700

    seamless footer 2

--- js/main.js
+++ js/main.js
@@ -246,7 +246,7 @@ $(document).ready(function () {
     $("#outputwrapper").html(msg);
   }
 
-  var bufferHeight = $('#header').height() + $('#footer').height() + 2;
+  var bufferHeight = $('#header').height() + $('#footer').height() + 10;
   $(window).resize(function() {
     $('#mainContent').height($(window).height() - bufferHeight);
   });
